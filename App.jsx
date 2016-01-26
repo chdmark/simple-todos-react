6cfac9a1d43807f8dec301660d46bc3c4ee22be5
Chris Mark <chdmark@gmail.com>
2016-01-25 18:02:03 -0800

    create simple todos react jsx file

---
+++ App.jsx
@@ -0,0 +1,11 @@
+App = React.createClass({
+
+	getTasks() {
+		return [
+			{ _id: 1, text: "This is task 1"},
+			{ _id: 2, text: "This is task 2"},
+			{ _id: 3, text: "this is task 3"},
+		]
+
+	}
+})
