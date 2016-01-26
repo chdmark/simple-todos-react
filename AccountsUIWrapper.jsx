c5868d06cb001ee5f638233ac1325d1798e61f9b
Chris Mark <chdmark@gmail.com>
2016-01-26 09:32:53 -0800

    Add react wrapper for accounts ui

---
+++ AccountsUIWrapper.jsx
@@ -0,0 +1,17 @@
+AccountsUIWrapper = React.createClass({
+
+	componentDidMount() {
+
+		this.view = Blaze.render(Template.loginButtons, 
+			React.findDOMNode(this.refs.container));
+	},
+
+	componentWillUnmount() {
+		Blaze.remove(this.view);
+	},
+
+	render() {
+
+		return <span ref="container" />
+	}
+})
