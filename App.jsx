701f34ba62ff9c6b54e1bab7befde0f94135b2a4
Chris Mark <chdmark@gmail.com>
2016-01-25 18:09:49 -0800

    Add app.jsx file

--- App.jsx
+++ App.jsx
@@ -5,7 +5,30 @@ App = React.createClass({
 			{ _id: 1, text: "This is task 1"},
 			{ _id: 2, text: "This is task 2"},
 			{ _id: 3, text: "this is task 3"},
-		]
+		];
 
+	}, 
+
+	renderTasks() {
+		return this.getTasks().map((task) => {
+			return <Task key={task._id} task={task} />;
+		});
+
+	},
+
+	render() {
+		return (
+			<div className="container">
+				<header>
+					<h1>Todo List</h1>
+				</header>
+
+				<ul>
+					{this.renderTasks()}
+				</ul>
+			</div>
+			);
 	}
-})
+
+
+});
