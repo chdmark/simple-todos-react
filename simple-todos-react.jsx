2ee96f6fece62dd4296ca73c7cf1e23ad0371383
Chris Mark <chdmark@gmail.com>
2016-01-26 17:52:50 -0800

    add extra security to methods

--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -49,6 +49,12 @@ Meteor.methods({
 },
 
 	removeTask(taskId) {
+		const task = Tasks.findOne(taskId);
+		if (task.private && task.owner !== Meteor.userId()) {
+
+			throw new Meteor.Error("not-authorized");
+		}
+
 		Tasks.remove(taskId);
 	},
 
